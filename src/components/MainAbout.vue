4a67042837887d7089084f8be1682a631f1aee67
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-22 12:08:56 +0100

    Aggiunto section jumbo e about

--- src/components/MainAbout.vue
+++ src/components/MainAbout.vue
@@ -1,6 +1,45 @@
 <template>
   <section>
 
+    <div class="container">
+
+      <div class="row">
+
+        <div class="container-about">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-trophy"></i>
+          </div>
+          <div class="description-wrapper">
+            <p class="title-about">Who we are</p>
+            <p class="description-about">Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.</p>
+            <p class="more-info">Learn more</p>
+          </div>
+        </div>
+        <div class="container-about">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-bolt"></i>
+          </div>
+          <div class="description-wrapper">
+            <p class="title-about">What we do</p>
+            <p class="description-about">Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.</p>
+            <p class="more-info">Learn more</p>
+          </div>
+        </div>
+        <div class="container-about">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-question"></i>
+          </div>
+          <div class="description-wrapper">
+            <p class="title-about">Why us?</p>
+            <p class="description-about">Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.</p>
+            <p class="more-info">Learn more</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
   </section>
 </template>
 
@@ -12,5 +51,50 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+section {
+
+  padding: 50px 0;
+
+  .row {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+  }
+
+  .container-about {
+    display: flex;
+    max-width: 30%;
+    gap: 25px;
+  }
+
+  .icon-wrapper {
+    color: $gray;
+  }
+
+  .description-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    .title-about {
+      text-transform: uppercase;
+      font-weight: 600;
+    }
+
+    .description-about {
+      color: $gray;
+    }
+
+    .more-info {
+      color: $froly;
+      font-style: italic;
+    }
+
+  }
+
+}
 
 </style>
--- src/components/MainJumbotron.vue
+++ src/components/MainJumbotron.vue
@@ -1,6 +1,32 @@
 <template>
   <section>
 
+    <div class="container">
+      <div class="row">
+
+        <div class="title-wrapper">
+          <h2 class="subtitle">Conference on</h2>
+          <h1 class="title">Social Networks</h1>
+        </div>
+
+        <div class="separator">
+          <figure>
+            <img src="../assets/img/1-item-2.jpg" alt="">
+          </figure>
+          <div class="date-wrapper">
+            <p class="date">23- 26 may 2019</p>
+            <p class="date">San Francisco</p>
+            <p class="date">8 Speakers</p>
+          </div>
+        </div>
+
+        <div class="button-jumbo">
+          <button class="button">Register online</button>
+        </div>
+
+      </div>
+    </div>
+
   </section>
 </template>
 
@@ -12,5 +38,79 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+section {
+
+  background-image: url('../assets/img/slider-1-bg.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: $white;
+  padding: 80px 0;
+
+  .row {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+
+    .title-wrapper {
+      text-transform: uppercase;
+      text-align: center;
+    }
+
+    .subtitle {
+      font-size: 45px;
+      font-weight: 100;
+      padding-bottom: 10px;
+    }
+
+    .title {
+      font-size: 60px;
+      font-weight: 700;
+    }
+
+    .separator {
+      text-align: center;
+    }
+
+    figure {
+      padding-bottom: 10px;
+    }
+
+    .date {
+
+      &::after {
+        content: ' /';
+      }
+    }
+
+    .date-wrapper {
+      display: flex;
+      justify-content: center;
+      gap: 20px;
+      font-size: 16px;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+
+    .button {
+      color: $white;
+      background-color: #f62b0a;
+      border: none;
+      text-transform: uppercase;
+      padding: 15px 25px;
+      border-radius: 3px;
+      font-weight: 600;
+      cursor: pointer;
+      margin-top: 5px;
+    }
+
+  }
+
+}
 
 </style>
