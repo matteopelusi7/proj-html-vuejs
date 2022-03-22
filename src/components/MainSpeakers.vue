6ed02d3b210e5a998e07f42c9403c7a4c077c81e
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-22 12:56:35 +0100

    Aggiunto section subscribe e speakers

--- src/components/MainSpeakers.vue
+++ src/components/MainSpeakers.vue
@@ -1,6 +1,67 @@
 <template>
   <section>
 
+    <div class="container">
+
+      <div class="row title-row">
+
+        <div class="title">
+          <h4>Speakers</h4>
+        </div>
+
+        <div class="icon-wrapper">
+          <i class="fa-solid fa-angle-left"></i>
+          <i class="fa-solid fa-angle-right"></i>
+        </div>
+
+      </div>
+
+      <div class="row speakers-row">
+
+        <div class="speakers-box">
+          <figure>
+            <img src="../assets/img/speaker-6-150x150.jpg" alt="">
+          </figure>
+          <div>
+            <p class="speaker">Patrick Spencer</p>
+            <p class="job">SeP Analyzer</p>
+          </div>
+        </div>
+
+        <div class="speakers-box">
+          <figure>
+            <img src="../assets/img/speaker-5-150x150.jpg" alt="">
+          </figure>
+          <div>
+            <p class="speaker">Janet Jones</p>
+            <p class="job">Newyork Post's GM</p>
+          </div>
+        </div>
+
+        <div class="speakers-box">
+          <figure>
+            <img src="../assets/img/speaker-4-150x150.jpg" alt="">
+          </figure>
+          <div>
+            <p class="speaker">Michael Dover</p>
+            <p class="job">Starbuck's CEO</p>
+          </div>
+        </div>
+
+        <div class="speakers-box">
+          <figure>
+            <img src="../assets/img/speaker-3-150x150.jpg" alt="">
+          </figure>
+          <div>
+            <p class="speaker">Angelina Holy</p>
+            <p class="job">Maxii's Manager</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
   </section>
 </template>
 
@@ -12,5 +73,67 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+section {
+
+  padding: 40px 0;
+
+  .speakers-row {
+    display: flex;
+    justify-content: center;
+    gap: 30px;
+
+    .speakers-box {
+      width: calc( 100% / 4);
+      text-align: center;
+
+      .speaker {
+        text-transform: uppercase;
+        font-weight: 600;
+        padding-bottom: 15px;
+      }
+
+      .job {
+        font-style: italic;
+        color: $gray;
+      }
+
+      img {
+        width: 250px;
+      }
+
+      figure {
+        padding-bottom: 15px;
+      }
+
+    }
+
+  }
+
+  .title-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 40px;
+
+    .title {
+      text-transform: uppercase;
+      font-size: 28px;
+    }
+
+    .icon-wrapper i {
+      padding: 8px 12px;
+      border-radius: 4px;
+      background-color: $red;
+      color: $white;
+      margin: 0 5px;
+      cursor: pointer;
+    }
+    
+  }
+
+}
 
 </style>
--- src/components/MainSubscribe.vue
+++ src/components/MainSubscribe.vue
@@ -1,6 +1,40 @@
 <template>
   <section>
 
+    <div class="container">
+
+      <div class="row">
+
+        <div class="countdown">
+          <div class="box-count">
+            <p class="num-count">3339</p>
+            <p class="type-count">Days</p>
+          </div>
+          <div class="box-count">
+            <p class="num-count">7</p>
+            <p class="type-count">Hours</p>
+          </div>
+          <div class="box-count">
+            <p class="num-count">28</p>
+            <p class="type-count">mins</p>
+          </div>
+          <div class="box-count">
+            <p class="num-count">17</p>
+            <p class="type-count">secs</p>
+          </div>
+        </div>
+        <div class="subscribe">
+          <p>Subscribe for updates</p>
+        </div>
+        <div class="form">
+          <input type="text" placeholder="Please fill your email">
+          <button>Subscribe</button>
+        </div>
+
+      </div>
+
+    </div>
+
   </section>
 </template>
 
@@ -12,5 +46,86 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+section {
+
+  background-image: url('../assets/img/coutdown-bg1.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 80px 0;
+
+  .row {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+
+    .subscribe {
+      text-transform: uppercase;
+      font-size: 20px;
+      font-weight: 600;
+    }
+
+    .form {
+
+      input {
+        width: 300px;
+        border: none;
+        padding: 15px 10px;
+        border-radius: 2px;
+        margin-right: 10px;
+      }
+
+      button {
+        color: $white;
+        background-color: #f62b0a;
+        border: none;
+        text-transform: uppercase;
+        padding: 15px 25px;
+        border-radius: 3px;
+        font-weight: 600;
+        cursor: pointer;
+        margin-top: 5px;
+      }
+
+    }
+
+    .countdown {
+      display: flex;
+      gap: 15px;
+
+      .box-count {
+        color: $white;
+        background-color: $black;
+        opacity: 0.6;
+        padding: 30px;
+        text-align: center;
+        border-radius: 5px;
+
+        .num-count {
+          font-size: 40px;
+          font-weight: 700;
+        }
+
+        .type-count {
+          text-transform: uppercase;
+          font-size: 12px;
+        }
+
+        &:first-child {
+          opacity: 0.8;
+        }
+
+      }
+
+    }
+
+  }
+
+}
 
 </style>
