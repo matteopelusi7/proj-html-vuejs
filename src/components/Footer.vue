dccafdf97f793f31a3b00de9e9d2986142e7cb07
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-23 16:32:23 +0100

    Aggiunto footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,140 @@
 <template>
   <footer>
 
+    <section class="footer-main">
+
+      <div class="container">
+
+        <div class="row">
+          
+          <FooterList :listItem="listItem" :imgItem="imgItem"/>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <section class="footer-final">
+
+      <div class="container">
+
+        <div class="row">
+          <p>&copy; Copyright GoodLayers All Right Reserved</p>
+          <p>About Us | Accomodation | Call For Paper | Contact Us</p>
+        </div>
+
+      </div>
+
+    </section>
+
   </footer>
 </template>
 
 <script>
+
+import FooterList from './FooterList.vue'
+
 export default {
   name: 'FooterContent',
-  
+  components: {
+    FooterList
+  },
+  data () {
+    return {
+      listItem: [
+        {
+          text: 'Sem porta mollis parturient'
+        },
+        {
+          text: 'Nullam lorem mattis purus'
+        },
+        {
+          text: 'Nibh sem sit ullamcorper'
+        },
+        {
+          text: 'Donec luctus imperdiet'
+        },
+        {
+          text: 'Magna pars studiorum'
+        },
+      ],
+      imgItem: [
+        {
+          img: require('../assets/img/photodune-7770665-two-white-coffee-mug-with-diy-decoration-o (1).jpg')
+        },
+        {
+          img: require('../assets/img/photodune-8523683-speaker-at-business-convention-and-present.jpg')
+        },
+        {
+          img: require('../assets/img/photodune-6745579-modern-creative-man-relaxing-on-workspace-.jpg')
+        },
+        {
+          img: require('../assets/img/photodune-8522811-speaker-at-business-conference-and-present (1).jpg')
+        },
+        {
+          img: require('../assets/img/photodune-8797753-multiethnic-people-with-startup-business-t (1).jpg')
+        },
+        {
+          img: require('../assets/img/card04-150x150.jpg')
+        },
+        {
+          img: require('../assets/img/photodune-6745606-man-wristwatch-calendar-key-and-glasses-on.jpg')
+        },
+        {
+          img: require('../assets/img/photodune-6745585-modern-creative-workspace-m-150x150.jpg')
+        },
+        {
+          img: require('../assets/img/6426608875_186070f8ca_o-150x150.jpg')
+        },
+        {
+          img: require('../assets/img/card01-150x150.jpg')
+        },
+        {
+          img: require('../assets/img/13349065283_64d09b067c_o-150x150.jpg')
+        },
+        {
+          img: require('../assets/img/photodune-8795110-overhead-of-essentials-denim-clothes-m-150.jpg')
+        },
+      ]
+    }
+  }
 }
+
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+footer {
+
+  .footer-main {
+    padding: 50px 0;
+    background-color: #212121;
+
+    .row {
+      display: flex;
+      justify-content: center;
+      gap: 40px;
+      color: $white;
+    }
+
+  }
+
+  .footer-final {
+    padding: 35px 0;
+    background-color: $black;
+    color: $gray;
+
+    .row {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 14px;
+    }
+  }
+
+}
 
 </style>
---
+++ src/components/FooterList.vue
@@ -0,0 +1,116 @@
+<template>
+
+  <div class="row">
+    <div class="box-footer">
+      <h4 class="title-footer">About Us</h4>
+      <p class="description-footer">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+      <div class="icon-footer">
+        <i class="fa-brands fa-facebook-square"></i>
+        <i class="fa-brands fa-twitter-square"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-google-plus-square"></i>
+        <i class="fa-brands fa-pinterest-square"></i>
+        <i class="fa-solid fa-square-share-nodes"></i>
+      </div>
+    </div>
+    <div class="box-footer">
+      <h4 class="title-footer">Recent News</h4>
+      <ul class="description-footer">
+        <li class="list" v-for="(item, i) in listItem" :key="i">
+          <i class="fa-solid fa-caret-right"></i>
+          {{ item.text }}
+        </li>
+      </ul>
+    </div>
+    <div class="box-footer">
+      <h4 class="title-footer">Get your seat!</h4>
+      <p class="description-footer">Book for your seat before it's too rate. We provide variety of ticket options.</p>
+      <button class="button">Check out tickets!</button>
+    </div>
+    <div class="box-footer">
+      <h4 class="title-footer">Gallery from last year</h4>
+      <ul class="img-wrapper">
+        <li class="img-list" v-for="(img, i) in imgItem" :key="i">
+          <img :src="img.img" alt="">
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'FooterList',
+  props: {
+    listItem: Array,
+    imgItem: Array
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import '../assets/scss/common.scss';
+
+.box-footer {
+
+  width: calc( (100% / 4) - 20px);
+
+  .title-footer {
+    text-transform: uppercase;
+    padding-bottom: 20px;
+  }
+
+  i {
+    cursor: pointer;
+  }
+
+  .description-footer {
+    color: $gray;
+    padding-bottom: 20px;
+  }
+
+  .icon-footer {
+    display: flex;
+    gap: 20px;
+    font-size: 19px;
+  }
+
+  .button {
+    color: $white;
+    background-color: $red;
+    border: none;
+    text-transform: uppercase;
+    padding: 10px 20px;
+    font-size: 12px;
+    border-radius: 3px;
+    font-weight: 600;
+    cursor: pointer;
+    margin-top: 5px;
+  }
+
+  .list {
+    text-transform: uppercase;
+    line-height: 22px;
+    font-size: 13px;
+
+    i {
+      padding-right: 5px;
+    }
+
+  }
+
+  .img-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 7px;
+  }
+
+  .img-list {
+    width: calc( (100% / 4) - 7px);
+  }
+
+}
+
+</style>
